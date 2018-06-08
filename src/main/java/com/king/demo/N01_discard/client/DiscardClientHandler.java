fd2a8869b4a07a75ee4a7dcecad6227155367f4b
jinlong <arowana.java@qq.com>
2018-06-08 21:20:00 +0800

    add: discard demo annotate

--- src/main/java/com/king/demo/N01_discard/client/DiscardClientHandler.java
+++ src/main/java/com/king/demo/N01_discard/client/DiscardClientHandler.java
@@ -6,6 +6,16 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.util.CharsetUtil;
 
+/**
+ * 与ChannelInboundHandlerAdapter的
+ * 区别就是, SimpleChannelInboundHandler在接收到数据后
+ * 会自动release掉数据占用的Bytebuffer资源
+ * (自动调用Bytebuffer.release())。
+ * 而为何服务器端不能用呢，
+ * 因为我们想让服务器把客户端请求的数据发送回去，
+ * 而服务器端有可能在channelRead方法返回前
+ * 还没有写完数据，因此不能让它自动release。
+ */
 public class DiscardClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
