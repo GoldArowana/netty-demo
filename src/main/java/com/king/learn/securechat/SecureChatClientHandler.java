49b111a811de91fba355ca13e739a086807e17c8
jinlong <arowana.java@qq.com>
2018-07-31 10:23:56 +0800

    update

---
+++ src/main/java/com/king/learn/securechat/SecureChatClientHandler.java
@@ -0,0 +1,15 @@
+package com.king.learn.securechat;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+/**
+ * Handles a client-side channel.
+ */
+public class SecureChatClientHandler extends SimpleChannelInboundHandler<String> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+        System.err.println(msg);
+    }
+}
